25839235a68af73e0fb0c396a5075be50de4d6c4
HongChinhh <htrinhpadu@gmail.com>
2017-09-25 04:12:29 +0700

    calculator
    chua hoan thanh

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        bool isTypingNumber = false;
+        
+         private void NhapSo(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            NhapSo(btn.Text);
+        }
+        private void NhapSo (string so)
+        {
+            if (isTypingNumber)
+                lblDisplay.Text = lblDisplay.Text + so;
+            else
+            {
+                lblDisplay.Text = so;
+                isTypingNumber = true;
+            }
+
+        }
+            
+    }
+}
+
